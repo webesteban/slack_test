7848d77d8cb476ccdb7077d1fc4ca2bafbf1695b
Esteban Hurtado <webesteban@hotmail.com>
2017-03-10 01:04:13 -0500

    get id_channel

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  post '/command'                         => 'slack_api#command'
+  post '/command'                         	  => 'slack_api#command'
+
+  post '/save_phone'                          => 'slack_api#save_phone'
+
+  post '/list_phones'                         => 'slack_api#list_phones'
+  
 end
