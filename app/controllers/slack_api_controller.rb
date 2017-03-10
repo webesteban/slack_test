fae5b364eb603bcdc26f222ac700422227b743e8
Esteban Hurtado <webesteban@hotmail.com>
2017-03-10 00:55:09 -0500

    get id_channel

--- app/controllers/slack_api_controller.rb
+++ app/controllers/slack_api_controller.rb
@@ -3,8 +3,23 @@ class SlackApiController < ApplicationController
 	
 	def command
 
+		response = "#{params[:channel_id]}/#{params[:channel_name]}/#{params[:channel_name]}"
 
-		render :text => params[:channel_id]
+		render :text => response
+  	end
+
+  	def save_phone
+
+		response = "#{params[:channel_id]}/#{params[:channel_name]}/#{params[:channel_name]}"
+
+		render :text => response
+  	end
+
+  	def list_phones
+
+		response = "#{params[:channel_id]}/#{params[:channel_name]}/#{params[:text]}"
+
+		render :text => response
   	end
 
 end
